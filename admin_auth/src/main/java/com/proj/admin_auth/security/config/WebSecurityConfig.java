7be1eaf5227df0394a41d59f47ff9c6709f02282
Dermi Malika <kikamalika976@gmail.com>
2021-05-09 23:21:31 +0200

    Updates authorities

--- admin_auth/src/main/java/com/proj/admin_auth/security/config/WebSecurityConfig.java
+++ admin_auth/src/main/java/com/proj/admin_auth/security/config/WebSecurityConfig.java
@@ -22,19 +22,19 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                    .antMatchers("/").permitAll()
                     .antMatchers("/api/v*/registration/**").permitAll()
-                    .antMatchers("/admin").permitAll()
-                    .antMatchers("/admin/**").hasRole("ADMIN")
-                    .antMatchers("/sadmin").permitAll()
-                    .antMatchers("/sadmin/**").hasRole("SUPERADMIN")
+                    .antMatchers("/admin/**").hasAuthority("ADMIN")
+                    .antMatchers("/sadmin/**").hasAuthority("SUPERADMIN")
+                    .antMatchers("/").permitAll()
 
-                   // .antMatchers("/api/v*/registration/**").permitAll()
+                    .antMatchers("/api/v*/registration/**").permitAll()
                     .anyRequest()
                     .authenticated()
                 .and()
                 .csrf().disable()
                 .formLogin();
+
+
 
     }
 
