158548ede3d6ae3d93e34ccc2842a35c16fbc6e3
Bellaouedj Ismail <i.bellaouedj@esi-sba.dz>
2021-08-09 20:08:46 +0100

    configuration done By Malika Upload Image in product

--- WORKS GOOD/e-meuble/spring-boot-jwt-jpa-master/src/main/java/com/techgeeknext/controller/ProductController.java
+++ WORKS GOOD/e-meuble/spring-boot-jwt-jpa-master/src/main/java/com/techgeeknext/controller/ProductController.java
@@ -1,8 +1,11 @@
 package com.techgeeknext.controller;
 
 import com.techgeeknext.dao.ProductRepository;
+import com.techgeeknext.entities.Admin;
 import com.techgeeknext.entities.Product;
+import com.techgeeknext.service.FileUploadUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -25,22 +28,48 @@ public class ProductController {
         return productRepository.findAll();
     }
 
+    /*
     @PostMapping("/upload")
     public void uploadImage(@RequestParam("imageFile") MultipartFile file) throws IOException {
         this.bytes = file.getBytes();
-    }
+    }*/
 
 
     @PostMapping("/add")
-    public void createProduct(@RequestBody Product product) throws IOException {
-        product.setPicByte(this.bytes);
-        productRepository.save(product);
-        this.bytes = null;
+    public void createProduct(@ModelAttribute Product product,@RequestParam("file") MultipartFile multipartFile) throws IOException {
+        //product.setPicByte(this.bytes);
+        String fileName = StringUtils.cleanPath(multipartFile.getOriginalFilename());
+        product.setFileUrl(fileName);
+       Product savedProduct = productRepository.save(product);
+        String uploadDir="product-photos/" + savedProduct.getId();
+        FileUploadUtil.saveFile(uploadDir, fileName, multipartFile);
+       // this.bytes = null;
     }
-    @PutMapping("/update")
-    public void updateProduct(@RequestBody Product product) {
-        productRepository.save(product);;
-    }
+    /*
+    @PutMapping("/update/{id}")
+    public Product updateProduct(@ModelAttribute Product product,@PathVariable("id") Long id,@RequestParam("file") MultipartFile multipartFile)throws IOException  {
+
+        if (productRepository.findById(id).isPresent()){
+            Product existingProduct = productRepository.findById(product.getId()).get();
+            String p = ;
+            p =StringUtils.cleanPath(multipartFile.getOriginalFilename());
+
+            existingProduct.setName(product.getName());
+            existingProduct.setCategory(product.getCategory());
+            existingProduct.setWeight(product.getWeight());
+            existingProduct.setQuantity(product.getQuantity());
+            existingProduct.setDescription(product.getDescription());
+            existingProduct.setPrice(product.getPrice());
+            existingProduct.setFileUrl(p);
+
+            Product updatedProduct = productRepository.save(existingProduct);
+
+            return updatedProduct;
+        }else{
+            return null;
+        }
+    }*/
+
 
 
     @CrossOrigin(origins = "http://localhost:4200")
