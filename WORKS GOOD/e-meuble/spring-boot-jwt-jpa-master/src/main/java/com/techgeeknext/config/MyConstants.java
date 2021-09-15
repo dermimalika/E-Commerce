91dbaa0ccefded1cb41d7bfdec9e829e8b43d2ae
Dermi Malika <kikamalika976@gmail.com>
2021-09-15 14:48:45 +0200

    My constants

--- WORKS GOOD/e-meuble/spring-boot-jwt-jpa-master/src/main/java/com/techgeeknext/config/MyConstants.java
+++ WORKS GOOD/e-meuble/spring-boot-jwt-jpa-master/src/main/java/com/techgeeknext/config/MyConstants.java
@@ -3,15 +3,15 @@ package com.techgeeknext.config;
 public class MyConstants {
 
     // Replace with your email here:
-    public static final String MY_EMAIL = "bellaouedj.ismail116@gmail.com";
+    public static final String MY_EMAIL = "dermimalika@gmail.com";
 
     // Replace password!!
     public static final String MY_PASSWORD = "ismail/-=22";
 
     // Text Admin Email!
-    public static final String Admin_EMAIL = "Welcome! Now u had a compte in our Web APP";
+    public static final String Admin_EMAIL = "Welcome! Now You Have an account in our Web APP";
 
     // Text Client Email!
-    public static final String Client_EMAIL = "Welcome! Now u had a compte in our Web APP \n Enjoy!!!!";
+    public static final String Client_EMAIL = "Welcome! Now You Have an account in our Web APP \n Enjoy!!!!";
 
 }
